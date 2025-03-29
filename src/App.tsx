2e991a063f5e7e420f0a951b3dba1af4e382b048
Vinicius Baptista <viniciusb_s@hotmail.com>
2025-03-29 11:57:49 -0300

    Criando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Teste from './teste'
+import EstiloGlobal from './styles'
+
+function App() {
+
+
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+
+  )
+}
+
+export default App
