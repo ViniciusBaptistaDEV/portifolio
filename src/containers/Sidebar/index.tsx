b30cb03c1a58f60d8a75f7a5433362883dca4e52
Vinicius Baptista <viniciusb_s@hotmail.com>
2025-03-31 22:40:33 -0300

    estilizando a Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,18 @@
+import Avatar from "../../components/Avatar";
+import Paragrafo from "../../components/Paragrafo";
 import Titulo from "../../components/Titulo";
+import { BotaoTema, Descricao, SidebarContainer } from "./styles";
 
 const Sidebar = () => (
     <aside>
-        <img src="https://github.com/viniciusbaptistadev.png" />
-        <Titulo fontSize={20}>Vinicius Baptista</Titulo>
+        <SidebarContainer>
+            <Avatar />
+            <Titulo fontSize={20}>Vinicius Baptista</Titulo>
+            <Paragrafo tipo="secundario" fontSize={16} >ViniciusBaptistaDEV</Paragrafo>
+            <Descricao tipo="principal" fontSize={12}>Desenvolvedor Front-end</Descricao>
+            <BotaoTema>Trocar tema</BotaoTema>
+
+        </SidebarContainer>
     </aside>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -11,6 +11,7 @@ const EstiloGlobal = createGlobalStyle`
 
     body {
         padding-top: 80px;
+        padding-bottom: 80px;
 
         @media (max-width: 768px) {
             padding-top: 16px;
@@ -35,7 +36,4 @@ export const Container = styled.div`
     display: block;
 }
 
-    img {
-    max-width: 100%;
-}
 `
